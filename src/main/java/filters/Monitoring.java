51b5fd384250db8b8c11088dad5dc28ec28545bc
henricker <henriquevieira@alu.ufc.br>
2021-06-12 20:45:08 -0300

    Added process execution monitoring filter.

--- src/main/java/filters/Monitoring.java
+++ src/main/java/filters/Monitoring.java
@@ -12,11 +12,18 @@ import javax.servlet.annotation.WebFilter;
 /**
  * Servlet Filter implementation class Monitoring
  */
-@WebFilter("/Monitoring")
+@WebFilter("/Business")
 public class Monitoring implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		Long before = System.currentTimeMillis();
 		
+		String action = request.getParameter("action");
+		
+		//execute action
 		chain.doFilter(request, response);
+		
+		Long after = System.currentTimeMillis();
+		System.out.println("Time of execution of the process " + action + " -> " + (after - before) + "ms");
 	}
 
 }
